bdea36bd41648c6e7820253bcf0ac3cc9248e03a
Andrey <lapinandreyly@bk.ru>
2021-04-03 13:37:24 +0300

    Initial Commit

---
+++ FruitsOnboarding/FruitsOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsOnboardingApp.swift
+//  FruitsOnboarding
+//
+//  Created by Андрей Лапин on 03.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct FruitsOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
